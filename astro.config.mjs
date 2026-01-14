9411ed2a74ba2e9fa5e984128cdc147157cdf36d
Swapnil Singh <swapnilsingh2005@yahoo.co.in>
2026-01-14 23:38:29 +0530

    Remove base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://swapnilsingh.github.io',
-  base: '/entitle-website',
+  site: 'https://swapnilsingh.github.io/entitle-website',
   output: 'static',
 });
