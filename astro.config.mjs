69645c941efb321e3b04a3521b34c21226e1f2ed
Swapnil Singh <swapnilsingh2005@yahoo.co.in>
2026-01-14 23:46:44 +0530

    Fix asset paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://swapnilsingh.github.io/entitle-website',
+  site: 'https://swapnilsingh.github.io',
+  base: '/entitle-website',
   output: 'static',
 });
