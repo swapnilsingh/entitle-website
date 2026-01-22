062901769f7091d1c522510e05ce59a233d9a120
Swapnil Singh <swapnilsingh2005@yahoo.co.in>
2026-01-22 13:56:31 +0530

    fix: update config for Cloudflare Tunnel and allowedHosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import viteConfig from './vite.config.js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://swapnilsingh.github.io',
-  base: '/entitle-website',
   output: 'static',
+  vite: viteConfig,
 });
